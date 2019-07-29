0f74f46bcaab909eb911fb19e4139fcd9d05bec2
Ruslan Povolockii <devianllert@gmail.com>
2019-07-29 16:04:22 +0300

    feat: add classnames package

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import React, { ReactChild, ReactElement } from 'react';
+import classnames from 'classnames';
+
+import styles from './Button.module.scss';
+
+interface Props {
+  disabled?: boolean;
+  children: ReactChild;
+}
+
+const Button = (props: Props): ReactElement => {
+  const { disabled, children } = props;
+
+  const classes = classnames(
+    styles.button,
+    disabled && styles.disabled,
+  );
+
+  return (
+    <button
+      className={classes}
+      type="button"
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
