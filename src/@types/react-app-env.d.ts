6b81769b960bdfb0c4d87b412bf36227734dc074
Ruslan Povolockii <devianllert@gmail.com>
2019-09-02 15:06:04 +0300

    chore: remove useless comments, add process.env vars to global types

--- src/@types/react-app-env.d.ts
+++ src/@types/react-app-env.d.ts
@@ -7,6 +7,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
+    readonly REACT_APP_VERSION: string;
+    readonly REACT_APP_API_SERVER: string;
   }
 }
 
--- src/configureStore.ts
+++ src/configureStore.ts
@@ -18,13 +18,6 @@ const configureStore = (initialState = {}, history: import('history').History): 
     if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
       // @ts-ignore
       composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
-
-    // NOTE: Uncomment the code below to restore support for Redux Saga
-    // Dev Tools once it supports redux-saga version 1.x.x
-    // if (window.__SAGA_MONITOR_EXTENSION__)
-    //   reduxSagaMonitorOptions = {
-    //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
-    //   };
     /* eslint-enable */
   }
 
