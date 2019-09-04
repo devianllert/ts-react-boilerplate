ce20b50827fdea06ab87311b278062ff7b83cbd0
Ruslan Povolockii <devianllert@gmail.com>
2019-09-04 20:35:12 +0300

    feat(hooks): add useWindowScroll hook

---
+++ src/hooks/useWindowScroll.ts
@@ -0,0 +1,56 @@
+import { useState, useEffect, useRef } from 'react';
+
+interface WindowScrollPosition {
+  x: number;
+  y: number;
+}
+
+const getWindowScrollPosition = (): WindowScrollPosition => ({ x: window.pageXOffset, y: window.pageYOffset });
+
+/**
+ * Hook that re-renders on window scroll.
+ */
+
+const useWindowScroll = (): WindowScrollPosition => {
+  const frame = useRef(0);
+  const [position, setScrollPosition] = useState<WindowScrollPosition>(getWindowScrollPosition());
+
+  useEffect((): (() => void) => {
+    const handleScroll = (): void => {
+      cancelAnimationFrame(frame.current);
+
+      frame.current = window.requestAnimationFrame((): void => {
+        setScrollPosition(getWindowScrollPosition());
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll, {
+      capture: false,
+      passive: true,
+    });
+
+    return (): void => {
+      cancelAnimationFrame(frame.current);
+
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return position;
+};
+
+const useWindowXScroll = (): number => {
+  const { x } = useWindowScroll();
+  return x;
+};
+
+const useWindowYScroll = (): number => {
+  const { y } = useWindowScroll();
+  return y;
+};
+
+export {
+  useWindowScroll,
+  useWindowXScroll,
+  useWindowYScroll,
+};
