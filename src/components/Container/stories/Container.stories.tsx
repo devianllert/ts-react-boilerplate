3076c77712816c4338a92e6d2cfe0ad1a9b1b3ff
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2020-04-27 21:05:00 +0300

    fix: fix container's stories

--- src/components/Container/stories/Container.stories.tsx
+++ src/components/Container/stories/Container.stories.tsx
@@ -8,17 +8,17 @@ export default {
 };
 
 export const Desktop = (): ReactElement => (
-  <Container maxWidth="desktop" style={{ backgroundColor: '#d4d4d4' }}>Primary Button</Container>
+  <Container maxWidth="desktop" style={{ backgroundColor: '#d4d4d4' }}>Desktop container</Container>
 );
 
 export const Laptop = (): ReactElement => (
-  <Container maxWidth="laptop" style={{ backgroundColor: '#d4d4d4' }}>Primary Button</Container>
+  <Container maxWidth="laptop" style={{ backgroundColor: '#d4d4d4' }}>Laptop container</Container>
 );
 
 export const Tablet = (): ReactElement => (
-  <Container maxWidth="tablet" style={{ backgroundColor: '#d4d4d4' }}>Primary Button</Container>
+  <Container maxWidth="tablet" style={{ backgroundColor: '#d4d4d4' }}>Tablet container</Container>
 );
 
 export const Mobile = (): ReactElement => (
-  <Container maxWidth="mobile" style={{ backgroundColor: '#d4d4d4' }}>Primary Button</Container>
+  <Container maxWidth="mobile" style={{ backgroundColor: '#d4d4d4' }}>Mobile container</Container>
 );
