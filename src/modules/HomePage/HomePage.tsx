b1585982c69fbb5e4ffa98ccbcf5a05cf9f00fe9
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2020-05-28 17:39:34 +0300

    feat: add interceptors, refresh tokens

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,12 +1,22 @@
 import React, { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useQuery } from 'react-query';
 import { Helmet } from 'react-helmet';
+
+import { getMe } from '../../services/users.service';
+
+import useAuth from '../../hooks/useAuth';
 
 import Typography from '../../components/Typography';
 import Container from '../../components/Container';
+import Button from '../../components/Button';
 
 const HomePage = (): ReactElement => {
   const { t } = useTranslation();
+  const { isAuthenticated, logout } = useAuth();
+  const { data } = useQuery(isAuthenticated && 'users/me', getMe, {
+    retry: false,
+  });
 
   return (
     <>
@@ -17,6 +27,13 @@ const HomePage = (): ReactElement => {
       <Container>
         <Typography variant="h1" align="center" gutterBottom>Home</Typography>
         <Typography variant="h2" align="center" paragraph>React typescript boilerplate</Typography>
+
+        {isAuthenticated && (
+          <>
+            <Typography>{data?.username}</Typography>
+            <Button onClick={logout}>Logout</Button>
+          </>
+        )}
       </Container>
     </>
   );
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -19,40 +19,23 @@ export interface UserSignUpDTO {
 
 export const setTokens = (data: Tokens): void => {
   localStorage.setItem('accessToken', data.accessToken);
-  localStorage.setItem('refreshToken', data.refreshToken);
-  localStorage.setItem('expiresIn', data.expiresIn);
 };
 
 export const clearTokens = (): void => {
   localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
-  localStorage.removeItem('expiresIn');
 };
 
 export const signUp = (payload: UserSignUpDTO): Promise<void> => api.post('auth/register', payload);
 
 export const login = async (payload: UserLoginDTO): Promise<void> => {
-  const { data } = await api.post<Tokens>('auth/login', payload);
+  const { data } = await api.post<Tokens>('auth/login', payload, { withCredentials: true });
 
   setTokens(data);
 };
 
 export const logout = async (): Promise<void> => {
-  const token = localStorage.getItem('refreshToken');
-
-  await api.post('auth/logout', { token })
+  await api.post('auth/logout', undefined, { withCredentials: true })
     .finally(clearTokens);
-};
-
-
-export const refreshTokens = async (): Promise<string> => {
-  const token = localStorage.getItem('refreshToken');
-
-  const { data } = await api.post<Tokens>('auth/refresh', { token });
-
-  setTokens(data);
-
-  return data.refreshToken;
 };
 
 export const verifyEmail = (token?: string): Promise<void> => api.post(`auth/verify/${token}`);
